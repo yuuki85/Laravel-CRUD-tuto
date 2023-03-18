cc3828c2b0985b92c8fcd7f516930f0ec216331b
yuuki85 <yuukikouhei85@gmail.com>
2023-03-18 11:45:17 +0800

    Video 10

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //pageinate guna untuk latest 5 products
+        $products   =   Product::latest()->paginate(5);
+
+        return view('products.index', compact('products'))->with(request()->input('page'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //validate the input
+        $request->validate([
+            'name'=>'required',
+            'detail'=>'required'
+        ]);
+
+        //create new product
+        Product::create($request->all());
+
+        //return to index page
+        return redirect()->route('products.index')->with('success','Product created successfully');
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
